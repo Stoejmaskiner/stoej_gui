d17bb2b067716df3b5d986993c4fd75818e860fb
Lorenzo Panieri <gokk99@gmail.com>
2023-02-26 01:34:00 +0100

    module header

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # stoej_gui
-GUI extensions for JUCE
+
+This is an optional feature of the stoej extensions for juce. You can see the
+main documentation in the [stoej_core]() repository.
---
+++ stoej_gui.h
@@ -0,0 +1,17 @@
+/*
+
+BEGIN_JUCE_MODULE_DECLARATION
+
+    ID:                     stoej_gui
+    vendor:                 stoej
+    version:                0.1.0
+    name:                   stoej_gui
+    description:            GUI extensions for the JUCE framework
+
+    dependencies:           stoej_core
+    license:                GPL3 / commercial
+    minimumCppStandard:     20
+
+END_JUCE_MODULE_DECLARATION
+
+*/
